4239462da805bffb1fd05de9566026845babab0e
Zoe Erotokritou <Zoe@Zoe-pc>
2014-03-20 17:24:26 +0000

    finish test for appear_last

--- test/test_appears_first.py
+++ test/test_appears_first.py
@@ -1,5 +1,17 @@
 import unittest
-from comp61542.statistics import appears_first
+from comp61542.statistics.appears_first import author_appears_first
 
 __author__ = 'zoe'
 
+class AppearsFirst(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_appear_first_is_zero_for_empty_dataset(self):
+           self.assertEqual(author_appears_first([]), 0)
+
+
+
+if __name__ == "__main__":
+ unittest.main()
