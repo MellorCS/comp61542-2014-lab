60acd84174773b9b30501ef8d9e08b433bc42848
Zoe Erotokritou <Zoe@Zoe-pc>
2014-03-21 15:50:59 +0000

    Change in the templates

--- test/test_appears_first.py
+++ test/test_appears_first.py
@@ -1,6 +1,7 @@
 import unittest
+from os import path
 from comp61542.statistics.appears_first import author_appears_first
-
+from comp61542.database import database
 __author__ = 'zoe'
 
 class AppearsFirst(unittest.TestCase):
@@ -11,7 +12,20 @@ class AppearsFirst(unittest.TestCase):
     def test_appear_first_is_zero_for_empty_dataset(self):
            self.assertEqual(author_appears_first([]), 0)
 
-
+    def temp(self):
+        db = database.Database()
+        self.assertTrue(db.read(path.join(self.data_dir, "simple.xml")))
+        header, data = db.get_publication_summary()
+        self.assertEqual(len(header), len(data[0]),
+            "header and data column size doesn't match")
+        self.assertEqual(len(data[0]), 6,
+            "incorrect number of columns in data")
+        self.assertEqual(len(data), 2,
+            "incorrect number of rows in data")
+        self.assertEqual(data[0][1], 1,
+            "incorrect number of publications for conference papers")
+        self.assertEqual(data[1][1], 2,
+            "incorrect number of authors for conference papers")
 
 if __name__ == "__main__":
  unittest.main()
