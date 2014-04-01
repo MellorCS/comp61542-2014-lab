a16468b59daae074e55c33624dc65676416a40a7
panosGel <panosgel@gmail.com>
2014-04-01 01:56:12 +0100

    added a new view for first, last and sole author by publication type.

--- test/test_get_results_of_search_author.py
+++ test/test_get_results_of_search_author.py
@@ -1,9 +1,10 @@
 __author__ = 'panos'
+__author__ = 'panos'
 import unittest
 from os import path
 from comp61542.database import database
 
-class getResultsOfSearch(unittest.TestCase):
+class getResultsOfSearchAuthor(unittest.TestCase):
 
     def setUp(self):
         dir, _ = path.split(__file__)
@@ -11,15 +12,14 @@ class getResultsOfSearch(unittest.TestCase):
 
     def test_get_results_of_search_author(self):
         db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "publications_small_sample.xml")))
-        data = db.get_results_of_search_author("s")
+        self.assertTrue(db.read(path.join(self.data_dir, "sprint2test.xml")))
+        data = db.get_results_of_search_name_author("Sam")
+        sortedData = db.get_results_of_search_author("Sam",data)
+        self.assertEqual(sortedData[0],"Sam Alice",
+            "incorrect sorted result")
+        self.assertEqual(sortedData[1],"Sam Brian",
+            "incorrect sorted result")
 
-        self.assertEqual(len(data),7,
-            "incorrect number of authors in the returned list")
-        data = db.get_results_of_search_author("Sean")
-        print len(data)
-        self.assertEqual(len(data),14,
-            "incorrect result when there is an exact match")
 
 
 
