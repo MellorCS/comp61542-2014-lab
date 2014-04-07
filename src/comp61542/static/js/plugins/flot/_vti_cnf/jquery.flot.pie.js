e2401dc35ff462eb24d33b284618039f4bad00f4
Zoe Erotokritou <Zoe@Zoe-pc>
2014-04-08 00:05:25 +0300

    New HTML design

--- src/comp61542/static/js/plugins/flot/_vti_cnf/jquery.flot.pie.js
+++ src/comp61542/static/js/plugins/flot/_vti_cnf/jquery.flot.pie.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|22 Mar 2014 19:14:55 -0000
+vti_timelastmodified:TR|06 Apr 2014 19:14:56 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_author:SR|Zoe-pc\\Zoe
 vti_modifiedby:SR|Zoe-pc\\Zoe
 vti_timecreated:TR|24 Oct 2013 22:15:34 -0000
-vti_nexttolasttimemodified:TR|21 Mar 2014 18:18:00 -0000
-vti_cacheddtm:TX|22 Mar 2014 19:14:54 -0000
+vti_nexttolasttimemodified:TR|06 Apr 2014 19:14:30 -0000
+vti_backlinkinfo:VX|n/flot.html
+vti_cacheddtm:TX|06 Apr 2014 19:14:29 -0000
 vti_filesize:IR|22589
-vti_backlinkinfo:VX|
--- src/comp61542/static/js/plugins/metisMenu/_vti_cnf/jquery.metisMenu.js
+++ src/comp61542/static/js/plugins/metisMenu/_vti_cnf/jquery.metisMenu.js
@@ -1,8 +1,8 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|22 Mar 2014 19:14:55 -0000
+vti_timelastmodified:TR|06 Apr 2014 19:14:57 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_modifiedby:SR|Zoe-pc\\Zoe
-vti_nexttolasttimemodified:TR|21 Mar 2014 18:18:00 -0000
-vti_cacheddtm:TX|22 Mar 2014 19:14:54 -0000
+vti_nexttolasttimemodified:TR|06 Apr 2014 19:14:31 -0000
+vti_backlinkinfo:VX|n/blank.html n/flot.html n/buttons.html n/notifications.html n/forms.html n/login.html n/morris.html n/panels-wells.html n/index.html n/grid.html n/tables.html n/typography.html n/Untitled_1.html
+vti_cacheddtm:TX|06 Apr 2014 19:14:29 -0000
 vti_filesize:IR|1386
-vti_backlinkinfo:VX|
