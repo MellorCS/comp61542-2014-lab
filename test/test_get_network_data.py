0145bc38580c1a481c6657b455eab6a54bd42d81
panosGel <panosgel@gmail.com>
2014-04-29 12:08:53 +0100

    fixed the tests for the graph

--- test/test_get_network_data.py
+++ test/test_get_network_data.py
@@ -16,8 +16,14 @@ class getNetworkData(unittest.TestCase):
         db = database.Database()
         self.assertTrue(db.read(path.join(self.data_dir, "publications_small_sample.xml")))
         graph = db.get_network_data()
-        print graph
 
-
+        self.assertEqual(len(graph[0]),14,
+            "Incorrect number of vertices in the network graph")
+        self.assertEqual(len(graph[1]),23,
+            "Incorrect number of vertices in the network graph")
+        self.assertTrue((1,3) in graph[1],
+            "Incorrect graph created")
+        self.assertTrue((0,3) in graph[1],
+            "Incorrect graph created")
 if __name__ == "__main__":
  unittest.main()
